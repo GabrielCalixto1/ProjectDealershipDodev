4771598aebb113645042c93cdd8e6f33b8d87165
Gabriel Calixto <gabrielcalixtosoares@gmail.com>
2022-06-02 17:48:11 -0700

    Changing Computer and Adding SeriLog

--- ProjectDealership.Web/DTOs/CarDTO.cs
+++ ProjectDealership.Web/DTOs/CarDTO.cs
@@ -2,6 +2,7 @@ namespace ProjectDealership.Web.DTOs
 {
     public class CarDTO : VehicleDTO
     {
+        //changes
         public bool AutomaticTransmission { get; set; }
         public string Fuel { get; set; }
     }
